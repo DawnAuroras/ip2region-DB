79b8d9b32731a2cc8c0ebe25aab4572d51dd74c5
DawnAuroras <140694074+DawnAuroras@users.noreply.github.com>
2023-07-27 15:30:53 +0800

    IP database file for ip2region dependency package

---
+++ ip_database.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.37-log : Database - world
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`world` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `world`;
+
+/*Table structure for table `ip_database` */
+
+DROP TABLE IF EXISTS `ip_database`;
+
+CREATE TABLE `ip_database` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(15) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `province` varchar(100) DEFAULT NULL,
+  `city` varchar(200) DEFAULT NULL,
+  `post_code` varchar(20) DEFAULT NULL,
+  `isp` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `ip_database` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
